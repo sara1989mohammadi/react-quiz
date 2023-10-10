c3b4c9e94f48c463d721434f02f9e132a78172f2
sara1989mohammadi <sara.mohammadi0612@gmail.com>
2023-10-11 00:19:52 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Header from "./Header";
+
 function App() {
   return (
-    <div className="app-header">
-      <img src="logo512.png" alt="Reactlogo" />
+    <div className="app">
+      <Header />
+      <main className="main">
+        <p>1/15</p>
+        <p>questions?</p>
+      </main>
     </div>
   );
 }
